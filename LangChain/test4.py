b6bed02018606007ef70dfc7d6dd8ae25d4db4c2
huang-qiruiqq <3208137151@qq.com>
2026-05-08 16:23:09 +0800

    使用 @tool 装饰器创建工具

--- LangChain/test4.py
+++ LangChain/test4.py
@@ -1,8 +1,36 @@
 from langchain_core.tools import tool
+from pydantic import BaseModel, Field
+from typing_extensions import Annotated
 
 # 定义工具
+
+# 方法一
+# @tool
+# def add(a: int, b: int) -> int:
+#     """两数相加
+#
+#     Args:
+#         a (int): a
+#         b (int): b
+#     """
+#     return a + b
+
+# 方法二
+# class AddInput(BaseModel):
+#     """两数相加"""
+#     a : int = Field(..., description="第一个整数")
+#     b : int = Field(..., description="第二个整数")
+#
+# @tool(args_schema=AddInput)
+# def add(a: int, b: int) -> int:
+#     return a + b
+
+# 方法三
 @tool
-def add(a: int, b: int) -> int:
+def add(
+        a: Annotated[int, ..., "第一个整数"],
+        b: Annotated[int, ..., "第二个整数"]
+) -> int:
     """两数相加
 
     Args:
@@ -10,7 +38,6 @@ def add(a: int, b: int) -> int:
         b (int): b
     """
     return a + b
-
 print(add.invoke({"a" : 2, "b" : 3}))
 print(add.name)
 print(add.description)
