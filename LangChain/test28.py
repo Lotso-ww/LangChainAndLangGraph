fd7e1d20af1f129778fa7ca663035aaeb2665a16
huang-qiruiqq <3208137151@qq.com>
2026-06-04 15:06:48 +0800

    基于字符长度拆分

---
+++ LangChain/test28.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+
+loader = UnstructuredMarkdownLoader(file_path="../DOC/md/脚手架级微服务租房平台Q&A.md")
+data = loader.load()
+
+text_spliter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=400,
+    chunk_overlap=50,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+documents = text_spliter.split_documents(data)
+for document in documents[:10]:
+    print("*" * 30)
+    print(document)
