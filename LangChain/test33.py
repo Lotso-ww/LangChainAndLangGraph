b4ee11f8758851de76725399086c1e56c7cb244d
huang-qiruiqq <3208137151@qq.com>
2026-06-12 09:03:12 +0800

    redis向量存储 -- CRUD

--- LangChain/test33.py
+++ LangChain/test33.py
@@ -1,5 +1,7 @@
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_redis import RedisVectorStore, RedisConfig
+from langchain_text_splitters import CharacterTextSplitter
 
 # 嵌入模型
 embeddings = DashScopeEmbeddings(
@@ -21,3 +23,40 @@ vector_store = RedisVectorStore(
     embeddings=embeddings,
     config=config,
 )
+
+# CRUD
+
+# single 模式，只生成一个大文档
+loader = UnstructuredMarkdownLoader("../Doc/md/脚手架级微服务租房平台Q&A.md",)
+# Document 列表
+data = loader.load()
+
+# tiktoken 分词器
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    encoding_name="cl100k_base", # cl100k_base 是tiktoken 分词器中的一种编码方式
+    chunk_size=400,              # 块token大小(参考标准，为了保证段落/句子完整，会超出此设定的大小)
+    chunk_overlap=50,            # 块重叠大小
+)
+
+# 文档列表
+docs = text_splitter.split_documents(data)
+for i, doc in enumerate(docs, start=1):
+    doc.metadata["category"] = "QA"
+    doc.metadata["num"] = i
+
+# 编制索引 -- 添加文档
+ids = vector_store.add_documents(docs)
+print(f"编制了{len(ids)}个索引")
+print(f"前三个索引是：{ids[:3]}")
+
+# 查
+# print(vector_store.get_by_ids(["01KTWNEF8M2E830475BCCYC61A"]))
+
+# 删除
+vector_store.delete(["01KTWNEF8M2E830475BCCYC61A"])
+print(vector_store.get_by_ids(["01KTWNEF8M2E830475BCCYC61A"]))
+
+# vector_store.index.drop_keys(["qa:01KTWNEF8M2E830475BCCYC61A"])
+
+# 全量删除
+# vector_store.index.delete(drop=True)
